fdfda155122f3caafe3329b6929d974b9803f586
deyog2228 <deyog2228@my.uwstout.edu>
2016-04-03 19:36:11 -0500

    Raycast for enemy

--- MazeMasterProject/Assets/Scripts/Player/PlayerRaycast.cs
+++ MazeMasterProject/Assets/Scripts/Player/PlayerRaycast.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class PlayerRaycast : MonoBehaviour {
     // intializes the score and then sets up the script variables to be called so we can change them to call animations.
 	public float fReachDistance = 2.0f;
+	public ParticleSystem enemyFire;
+	public GameObject enemy;
 	private GameObject cam;
+	private float fireTimer;
 
     // Use this for initialization
     void Start ()
     {
 		cam = transform.FindChild ("Camera").gameObject;
+		fireTimer = 0.0f;
+		enemyFire.Pause ();
     }
 
     // Update is called once per frame
@@ -22,34 +27,57 @@ public class PlayerRaycast : MonoBehaviour {
 		{	
 			//print(hit.collider.name);
             // if the mouse is down it will check for a collider of the object and then from the collider pull the game object and the tag then adds points based on tag and destroys object.
-			if (Input.GetKey(KeyCode.E))
-            {
-				switch(hit.collider.gameObject.tag){
+			//switch (hit.collider.gameObject.tag) {
+			/*case "Enemy":
+				if (Input.GetKey (KeyCode.E)) {
+					fireTimer += Time.deltaTime;
+					if (fireTimer >= 5.0f) {
+						print ("hello");
+					}
+				} else {
+					fireTimer = 0.0f;
+				}
+			default:
+				break;*/
+
+			if (Input.GetKey (KeyCode.E)) {
+				switch (hit.collider.gameObject.tag) {
 				case "goldCoin":
-					collect(1,hit);
+					collect (1, hit);
 					break;
 				case "ingot":
-					collect(5,hit);
+					collect (5, hit);
 					break;
 				case "Gem":
-					collect(20,hit);
+					collect (20, hit);
 					break;
 				case "LootHolder":
-					GameObject.FindWithTag("HUD").GetComponent<HudScript>().addStoredLoot(GameObject.FindWithTag("HUD").GetComponent<HudScript>().Score);
-					collect(-GameObject.FindWithTag("HUD").GetComponent<HudScript>().Score,hit);
+					GameObject.FindWithTag ("HUD").GetComponent<HudScript> ().addStoredLoot (GameObject.FindWithTag ("HUD").GetComponent<HudScript> ().Score);
+					collect (-GameObject.FindWithTag ("HUD").GetComponent<HudScript> ().Score, hit);
 					break;
 				case "Safe":
 					// if the collider is tied to safe tagged game object it will change the bool "Looted" in "ChestLogic>" script to true to open the safe
-					 hit.collider.gameObject.GetComponent<ChestLogic>().Looted = true;
+					hit.collider.gameObject.GetComponent<ChestLogic> ().Looted = true;
 					break;
 				case"door":
-						hit.collider.gameObject.GetComponent<DoorWayScript>().StartAnime();
+					hit.collider.gameObject.GetComponent<DoorWayScript> ().StartAnime ();
+					break;
+				case "Enemy":
+					fireTimer += Time.deltaTime;
+					print (fireTimer);
+					if (fireTimer >= 3) {
+						Invoke ("Kill", 5);
+						setAblaze ();
+					}
 					break;
 				default:
 					break;
 				} //End switch(hit.collider.gameObject.tag)
-            }
+			} else {
+				fireTimer = 0.0f;
+			}
 		} //End if (Physics.Raycast(ray, out hit))   
+
     }//End void Update()
 
 	//TODO: enter function header
@@ -62,5 +90,13 @@ public class PlayerRaycast : MonoBehaviour {
 			Destroy (pHit.collider.gameObject);
 		}
 	} //End private void collect(int pPoints, RaycastHit pHit)
+
+	void setAblaze(){
+		enemyFire.Play ();
+	}
+
+	void Kill(){
+		enemy.SetActive (false);
+	}
 } //End public class lootScriptGold : MonoBehaviour
 
